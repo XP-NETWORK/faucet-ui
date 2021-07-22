8d55803e6915d4a4a2a0e52e2a2c2f08dfdf75eb
Dima <dima@xp.network>
2021-07-22 20:59:17 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,180 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from 'react';
+import XPLogo from './assets/SVG/XPLogo';
+import {
+  XPApp,
+  XPMain,
+  XPBoxCenter,
+  XPFlexCenter,
+  XPTitle,
+  XPRow,
+  XPColumn,
+  XPLabel,
+  XPTransaction
+} from './StyledComponents';
+
+import SendButton from './SendButton';
+import XPTable, { XPTableStyles } from './XPTable';
+
+const maxAmount = 2000000000000000;
+const url = 'http://localhost:6644'
 
 function App() {
+
+  const [sendInactive, setSendInactive] = useState(false);
+  const [amount, setAmount] = useState(maxAmount);
+  const [account, setAccount] = useState('');
+  const [count, setCount] = useState(10);
+  const [data, setData] = useState([]);
+
+  fetch(`${url}/history/${count + 1}`)
+        .then(d => d.json())
+        .then(json => {
+          setData(json)
+        })
+
+  const columns = useMemo(() => [
+    {
+      Header: 'Timestamp',
+      accessor: 'timestamp'
+    },
+    {
+      Header: 'Target Address',
+      accessor: 'address'
+    },
+    {
+      Header: 'XPNET',
+      accessor: 'value'
+    },
+
+  ])
+
+  const handleSendButtonClick = async () => {
+    setSendInactive(true);
+
+    const rawResponse = await fetch(`${url}/transfer`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ "target": account, "amount": amount })
+    });
+    const content = await rawResponse.json();
+
+    console.log(content);
+
+    if (content) {
+      setSendInactive(false);
+      setCount(count + 1);
+
+      fetch(`${url}/history/${count + 1}`)
+        .then(d => d.json())
+        .then(json => {
+          setData(json)
+        })
+
+    }
+  }
+
+  const numberWithCommas = (x) => {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+      x = x.replace(pattern, "$1,$2");
+    return x;
+  }
+
+  const handleChangeAmount = (e) => {
+
+    if (e.target.value && e.target.value <= maxAmount) {
+      setAmount(e.target.value);
+    } else {
+      setAmount(maxAmount);
+    }
+  }
+
+
+  const handleAccountChange = (e) => {
+    setAccount(e.target.value);
+  }
+
+
+  // ==========================================================
+  //                            J S X
+  // ==========================================================
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <XPApp>
+      <XPMain>
+        <XPBoxCenter>
+          <XPFlexCenter>
+
+            <XPLogo />
+            <XPTitle>Testnet Faucet</XPTitle>
+
+            {/* -------------------------------------------- */}
+            {/* ---------- The first Row of elements ------- */}
+            {/* -------------------------------------------- */}
+
+            <XPRow>
+              <XPLabel>Faucet balance:</XPLabel>
+              <XPTransaction
+                value={numberWithCommas(200000000000000000)}
+                disabled
+              ></XPTransaction>
+            </XPRow>
+            <XPRow>
+              <XPColumn></XPColumn>
+            </XPRow>
+            <XPRow>
+              <XPLabel>Transfer Amount:</XPLabel>
+              <XPTransaction
+                value={amount}
+                onChange={handleChangeAmount}
+              ></XPTransaction>
+            </XPRow>
+            <XPRow>
+              <XPColumn></XPColumn>
+            </XPRow>
+
+            {/* -------------------------------------------- */}
+            {/* --------- The second Row of elements ------- */}
+            {/* -------------------------------------------- */}
+
+            <XPRow>
+              <XPLabel>Target Account:</XPLabel>
+              <XPTransaction
+                value={account}
+                onChange={handleAccountChange}
+              ></XPTransaction>
+
+            </XPRow>
+
+            <XPRow>
+              <XPColumn></XPColumn>
+            </XPRow>
+
+            {/* -------------------------------------------- */}
+            {/* --------------- The Send Button ------------ */}
+            {/* -------------------------------------------- */}
+
+            <SendButton onClick={handleSendButtonClick} inactive={sendInactive} />
+
+            {/* -------------------------------------------- */}
+            {/* ------------------ TX Table ---------------- */}
+            {/* -------------------------------------------- */}
+
+            <XPRow>
+              <XPColumn></XPColumn>
+            </XPRow>
+
+            <XPTableStyles>
+              <XPTable columns={columns} data={data} />
+            </XPTableStyles>
+
+          </XPFlexCenter>
+        </XPBoxCenter>
+      </XPMain>
+    </XPApp>
   );
 }
 
---
+++ src/XPTable.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import styled from 'styled-components'
+import { useTable } from 'react-table'
+
+const borderColor = "#374462";
+const borderRadius = "6px";
+
+export const XPTableStyles = styled.div`
+
+  table {
+    border-spacing: 0;
+    border: 1px solid ${borderColor};
+    width: 100%;
+    border-radius: ${borderRadius};
+    font-size: 12px;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid ${borderColor};
+      border-right: 1px solid ${borderColor};
+      opacity: 0.6;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+const getFullTimestamp = (timestamp) => {
+    const pad = (n,s=2) => (`${new Array(s).fill(0)}${n}`).slice(-s);
+    const d = new Date(timestamp);
+    
+    return `${pad(d.getFullYear(),4)}-${pad(d.getMonth()+1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
+  }
+
+const XPTable = ({columns, data}) => {
+
+    data = data.map(record => {
+        return {... record, 
+            timestamp: getFullTimestamp(record.timestamp),
+            value: record.value / 1000000000000000
+        }
+    });
+
+    data.reverse();
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({
+        columns,
+        data,
+      })
+
+    return (
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      )
+}
+
+export default XPTable;
